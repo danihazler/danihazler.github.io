7d35124ad1077617ee71cb5cef2cb19532609908
Dani <danihazler@gmail.com>
2018-08-31 16:14:51 +0100

    Update flower-shop project

--- flower-shop/resources/js/main.js
+++ flower-shop/resources/js/main.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
+  $('#galleryCarousel').carousel({
+    interval: 5000
+  });
+  //  ------- CAROUSEL GALLERY ------
+  //Handles the carousel thumbnails
+  $('[id^=carousel-selector-]').click(function () {
+    var id_selector = $(this).attr("id");
+    try {
+      var id = /-(\d+)$/.exec(id_selector)[1];
+      console.log(id_selector, id);
+      jQuery('#galleryCarousel').carousel(parseInt(id));
+    } catch (e) {
+      console.log('Regex failed!', e);
+    }
+  });
+  // When the carousel slides, auto update the image
+  $('#galleryCarousel').on('slid.bs.carousel', function (e) {
+    var id = $('.item.active').data('slide-number');
+    $('#carousel-text').html($('#slide-content-'+id));
+  });
   //  ------- NAVIGATION SCROLL ------
-$('a[href*="#"]')
+  $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
     if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      && location.hostname == this.hostname) {
+    && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
@@ -18,15 +38,15 @@ $('a[href*="#"]')
   });
   //  ----------- SCROLL TO THE TOP ------------
   $(window).scroll(function(){
-		if ($(this).scrollTop() > 300) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
+    if ($(this).scrollTop() > 300) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+  //Click event to scroll to top
+  $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
 });
